42cfc46e72a12f73b15236d727a9ee303040815f
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2023-01-17 02:47:37 +0300

    style story section and install tailwind scroll bar

--- components/Stories.jsx
+++ components/Stories.jsx
@@ -17,7 +17,7 @@ const Stories = () => {
         console.log(storyUsers);
     },[])
   return (
-      <div>
+      <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border overflow-x-scroll rounded-sm scrollbar-none">
           {storyUsers.map((user) => (
               <Story key={user.id} username={user.username} img={user.img} />
           ))}
--- components/Story.jsx
+++ components/Story.jsx
@@ -2,9 +2,9 @@
 
 const Story = ({img,username}) => {
   return (
-      <div>
-          <img src={img} alt={username} />
-          <p>{username}</p>
+      <div className="">
+          <img className="h-14 rounded-full p-[1.5px] border-red-500 border-2 cursor-pointer hover:scale-110 transition-transform duration-200 ease-out" src={img} alt={username} />
+          <p className="text-xs w-14 truncate">{username}</p>
     </div>
   )
 }

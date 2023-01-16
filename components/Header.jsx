586c68637eb900b782cc328aaf4e1df3fee115c7
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2023-01-17 00:28:54 +0300

    add search bar to the header component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,34 +1,37 @@
-import Image from 'next/image'
-import React from 'react'
-
+import Image from "next/image";
+import React from "react";
+import { SearchIcon } from "@heroicons/react/outline";
 const Header = () => {
   return (
-      <div>
-          {/* Left */}
-          
-          <div className="flex items-center justify-between max-w-6xl">
-              <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
-                  <Image
-                      className='object-contain'
-                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1280px-Instagram_logo.svg.png"
-                      layout="fill"
-                  />
-              </div>
-              <div className="cursor-pointer h-24 w-10 relative lg:hidden">
-                  <Image
-                      className='object-contain'
-                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png"
-                      layout="fill"
-                  />
-              </div>
-              <h1>Right side</h1>
-            </div>
+    <div className="flex items-center justify-between max-w-6xl">
+      {/* Left */}
 
-          {/* Middle */}
+      <div className="cursor-pointer h-24 w-24 relative hidden lg:inline-grid">
+        <Image
+          className="object-contain"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1280px-Instagram_logo.svg.png"
+          layout="fill"
+        />
+      </div>
+      <div className="cursor-pointer h-24 w-10 relative lg:hidden">
+        <Image
+          className="object-contain"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png"
+          layout="fill"
+        />
+      </div>
+      {/* Middle */}
+      <div className="relative mt-1">
+        <div className="absolute top-2 left-2">
+          <SearchIcon className="h-5 text-gray-500" />
+        </div>
+        <input type="text" placeholder="Search" className="bg-gray-50 pl-10 border-gray-500 text-sm focus:ring-black focus:border-black rounded-md" />
+      </div>
 
-          {/* Right */}
+      {/* Right */}
+      <h1>Right side</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;

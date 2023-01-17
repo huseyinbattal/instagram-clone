da3603d7d70e464d88d03be743023e40dffad609
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2023-01-17 11:09:36 +0300

    create the buttons section of the post component

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,4 +1,4 @@
-import { DotsHorizontalIcon } from "@heroicons/react/outline"
+import { DotsHorizontalIcon, HeartIcon, ChatIcon,BookmarkIcon } from "@heroicons/react/outline"
 
 const Post = ({id,username,userImg,img,caption}) => {
   return (
@@ -23,6 +23,16 @@ const Post = ({id,username,userImg,img,caption}) => {
               src={img}
               alt=""
           />
+
+        {/* Post Buttons */}
+
+          <div className="flex justify-between px-4 pt-4">
+              <div className="flex space-x-4">
+                  <HeartIcon className="btn" />
+                  <ChatIcon className="btn"/>
+              </div>
+              <BookmarkIcon className="btn"/>
+          </div>
 
     </div>
   )

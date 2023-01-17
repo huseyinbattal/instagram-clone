1ad1dfe3bb9cac55e7aed61eb2422e3fbdaad1fd
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2023-01-17 10:44:33 +0300

    create the header and image sections of the post component

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,9 +1,29 @@
-
+import { DotsHorizontalIcon } from "@heroicons/react/outline"
 
 const Post = ({id,username,userImg,img,caption}) => {
   return (
-      <div>
-          <h1>{username}</h1>
+      <div className="bg-white my-7 border rounded-md">
+          
+          {/* Post Header */}
+
+          <div className="flex items-center p-5">
+              <img
+                  className="h-12 rounded-full object-cover border p-1 mr-3"
+                  src={userImg}
+                  alt={username}
+              />
+              <p className="font-bold flex-1">{username}</p>
+              <DotsHorizontalIcon className="h-5"/>
+          </div>
+
+          {/* Post Image */}
+
+          <img
+              className="object-cover w-full"
+              src={img}
+              alt=""
+          />
+
     </div>
   )
 }
--- components/Posts.jsx
+++ components/Posts.jsx
@@ -5,14 +5,14 @@ const Posts = () => {
   const posts = [
     {
       id: "1",
-      username: "codewithsahand",
+      username: "codewithbattal",
       userImg: "https://media.licdn.com/dms/image/C4D03AQFcRdc7CNsYfw/profile-displayphoto-shrink_800_800/0/1654269434464?e=2147483647&v=beta&t=oVKynO5g8Jkqdwg5_Ug6GUI2FEvUL7urEROUeVFTcjg",
       img: "https://images.unsplash.com/photo-1673725437510-fe7bc808c306?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
       caption:"Nice picture"
     },
     {
       id: "2",
-      username: "ghavidelsahand",
+      username: "huseyinbattal",
       userImg: "https://media.licdn.com/dms/image/C4D03AQFcRdc7CNsYfw/profile-displayphoto-shrink_800_800/0/1654269434464?e=2147483647&v=beta&t=oVKynO5g8Jkqdwg5_Ug6GUI2FEvUL7urEROUeVFTcjg",
       img: "https://images.unsplash.com/photo-1661956602153-23384936a1d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
       caption:"New picture from my home"

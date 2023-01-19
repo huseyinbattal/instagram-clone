8b49fab6651082b2982646eb0842c94247825a87
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2023-01-19 21:28:58 +0300

    install recoil and add atom and UploadModel component

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import React from "react";
 import { SearchIcon, PlusCircleIcon } from "@heroicons/react/outline";
 import { HomeIcon } from "@heroicons/react/solid";
-import { useSession,signIn,signOut } from "next-auth/react";
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useRecoilState } from "recoil";
+import { modalState } from "../atom/modalAtom";
 
 const Header = () => {
   const { data: session } = useSession();
-  console.log(session);
+  const [open, setOpen] = useRecoilState(modalState);
   return (
     <div className="shadow-sm border-b sticky top-0 bg-white z-30">
       <div className="flex items-center justify-between max-w-6xl mx-4 xl:mx-auto">
@@ -45,18 +47,20 @@ const Header = () => {
           <HomeIcon className="hidden md:inline-flex h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
           {session ? (
             <>
-             <PlusCircleIcon className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out" />
+              <PlusCircleIcon
+                onClick={() => setOpen(true)}
+                className="h-6 cursor-pointer hover:scale-125 transition-transform duration-200 ease-out"
+              />
               <img
                 onClick={signOut}
-            src={session.user.image}
-            alt="user-image"
-            className="h-10 rounded-full cursor-pointer"
-          />
+                src={session.user.image}
+                alt="user-image"
+                className="h-10 rounded-full cursor-pointer"
+              />
             </>
           ) : (
-              <button onClick={signIn}>Sign in</button>
+            <button onClick={signIn}>Sign in</button>
           )}
-         
         </div>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import Header from '@/components/Header'
 import Feed from '@/components/Feed'
+import UploadModal from '@/components/UploadModal'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,7 +24,7 @@ export default function Home() {
       <Feed/>
 
       {/* Modal */}
-
+      <UploadModal/>
 
     </div>
   )

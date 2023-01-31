38e941639d79ff57dd6803e626ae0b71802d71b7
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2023-01-31 19:44:04 +0300

    add comment to the firebase

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,67 +1,91 @@
-import { DotsHorizontalIcon, HeartIcon, ChatIcon, BookmarkIcon, EmojiHappyIcon } from "@heroicons/react/outline";
+import {
+  DotsHorizontalIcon,
+  HeartIcon,
+  ChatIcon,
+  BookmarkIcon,
+  EmojiHappyIcon,
+} from "@heroicons/react/outline";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { useSession } from "next-auth/react";
+import { useState } from "react";
+import { db } from "../firebase";
 
 const Post = ({ id, username, userImg, img, caption }) => {
   const { data: session } = useSession();
+  const [comment, setComment] = useState("");
+
+  async function sendComment(event) {
+    event.preventDefault();
+    const commentToSend = comment;
+    setComment("");
+    await addDoc(collection(db, "posts", id, "comments"), {
+      comment: commentToSend,
+      username: session.user.username,
+      userImage: session.user.image,
+      timeStamp: serverTimestamp(),
+    });
+  }
   return (
-      <div className="bg-white my-7 border rounded-md">
-          
-          {/* Post Header */}
+    <div className="bg-white my-7 border rounded-md">
+      {/* Post Header */}
 
-          <div className="flex items-center p-5">
-              <img
-                  className="h-12 rounded-full object-cover border p-1 mr-3"
-                  src={userImg}
-                  alt={username}
-              />
-              <p className="font-bold flex-1">{username}</p>
-              <DotsHorizontalIcon className="h-5"/>
-          </div>
+      <div className="flex items-center p-5">
+        <img
+          className="h-12 rounded-full object-cover border p-1 mr-3"
+          src={userImg}
+          alt={username}
+        />
+        <p className="font-bold flex-1">{username}</p>
+        <DotsHorizontalIcon className="h-5" />
+      </div>
 
-          {/* Post Image */}
+      {/* Post Image */}
 
-          <img
-              className="object-cover w-full"
-              src={img}
-              alt=""
-          />
+      <img className="object-cover w-full" src={img} alt="" />
 
       {/* Post Buttons */}
-      
-      {session && (
 
-          <div className="flex justify-between px-4 pt-4">
-              <div className="flex space-x-4">
-                  <HeartIcon className="btn" />
-                  <ChatIcon className="btn"/>
-              </div>
-              <BookmarkIcon className="btn"/>
+      {session && (
+        <div className="flex justify-between px-4 pt-4">
+          <div className="flex space-x-4">
+            <HeartIcon className="btn" />
+            <ChatIcon className="btn" />
           </div>
+          <BookmarkIcon className="btn" />
+        </div>
       )}
 
+      {/* Post comments */}
 
-          {/* Post comments */}
-          
-          <p className="p-5 truncate"> <span className="font-bold mr-2">{username}</span> {caption} </p>
-          
+      <p className="p-5 truncate">
+        {" "}
+        <span className="font-bold mr-2">{username}</span> {caption}{" "}
+      </p>
+
       {/* Post input box */}
-      
+
       {session && (
-
-          <form className="flex items-center p-4">
-              <EmojiHappyIcon className="h-7"/>
-              <input
-                  className="border-none flex-1 focus:ring-0"
-                type="text"
-                placeholder="Enter your comment"
-              />
-              <button className="text-blue-400 font-bold">Post</button>
-            </form>
+        <form className="flex items-center p-4">
+          <EmojiHappyIcon className="h-7" />
+          <input
+            onChange={(event) => setComment(event.target.value)}
+            value={comment}
+            className="border-none flex-1 focus:ring-0"
+            type="text"
+            placeholder="Enter your comment"
+          />
+          <button
+            type="submit"
+            onClick={sendComment}
+            disabled={!comment.trim()}
+            className="text-blue-400 font-bold disabled:text-blue-200"
+          >
+            Post
+          </button>
+        </form>
       )}
-
-          
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
